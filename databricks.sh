ec0cc8b70bb165507ec51ad9ac85bc3accea7f03
Chrisewon <76791640+Chrisewon@users.noreply.github.com>
2022-06-22 22:45:09 +0530

    Update databricks.sh

--- databricks.sh
+++ databricks.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u 89XEDCwvMEVGG9XLcy3vA4EoGaLNFFHftJFjTWAjXQiqM8WD6EgXTAmZ7jK7nB7U6miDRnttNALMQW1U3Q3eZsj2FmDCCxs -k --tls --rig-id aank
